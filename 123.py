fbdf5a74d59eb13412856d22facc6d0ba162ee36
Nathan Yin <yinzeqi11@gmail.com>
2020-04-20 19:07:27 +1200

    Testing

---
+++ 123.py
@@ -0,0 +1,8 @@
+rain = input('Is it raining today?: ')
+if rain == 'Yes':
+	print('Stay at home, don\'t go outside')
+	print('Watching movies at home')
+
+if rain != 'Yes':
+	print('Please go out and buy some chips')
+	print('Money in the drawer')
